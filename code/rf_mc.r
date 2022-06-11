2905e8de772ddde331dbdb6c024cfc473db9a927
George Ostrouchov <ostrouchovg@ornl.gov>
2022-06-11 16:49:52 +0200

    nc

--- code/rf_mc.r
+++ code/rf_mc.r
@@ -10,6 +10,7 @@ train = LetterRecognition[-i_test, ]
 test = LetterRecognition[i_test, ]
 
 nc = as.numeric(commandArgs(TRUE)[2])                    #<<
+cat(nc, class(nc), "\n")
 ntree = lapply(splitIndices(500, nc), length)            #<<
 rf = function(x, train) randomForest(lettr ~ ., train, ntree=x, #<<
                                      norm.votes = FALSE)        #<<
