8b80808f44d9c9906b4eb11bfd4faa73bbf8627a
George Ostrouchov <ostrouchovg@ornl.gov>
2022-06-11 23:19:13 +0200

    time scalability

--- mpi/mnist_rsvd.sh
+++ mpi/mnist_rsvd.sh
@@ -15,4 +15,10 @@ echo "loaded R"
 module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore-11.2.0 
 export UCX_LOG_LEVEL=error
 
+time mpirun --map-by ppr:1:node Rscript mnist_rsvd.R
+time mpirun --map-by ppr:2:node Rscript mnist_rsvd.R
+time mpirun --map-by ppr:4:node Rscript mnist_rsvd.R
+time mpirun --map-by ppr:8:node Rscript mnist_rsvd.R
 time mpirun --map-by ppr:16:node Rscript mnist_rsvd.R
+time mpirun --map-by ppr:32:node Rscript mnist_rsvd.R
+
