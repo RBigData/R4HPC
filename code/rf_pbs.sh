7a4873b36e9387042621df3803e6d936f42dd933
George Ostrouchov <ostrouchovg@ornl.gov>
2022-06-12 14:50:07 +0200

    run one

--- code/rf_pbs.sh
+++ code/rf_pbs.sh
@@ -12,12 +12,12 @@ pwd
 module load R
 echo "loaded R"
 
-time Rscript rf_serial.r
-time Rscript rf_mc.r --args 1
-time Rscript rf_mc.r --args 2
-time Rscript rf_mc.r --args 4
+#time Rscript rf_serial.r
+#time Rscript rf_mc.r --args 1
+#time Rscript rf_mc.r --args 2
+#time Rscript rf_mc.r --args 4
 time Rscript rf_mc.r --args 8
-time Rscript rf_mc.r --args 16
-time Rscript rf_mc.r --args 32
-time Rscript rf_mc.r --args 64
-time Rscript rf_mc.r --args 128
+#time Rscript rf_mc.r --args 16
+#time Rscript rf_mc.r --args 32
+#time Rscript rf_mc.r --args 64
+#time Rscript rf_mc.r --args 128
