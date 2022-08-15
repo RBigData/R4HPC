ce149be2181c753ab70cdac319a0f63dcd419a65
go-ski <ostrouchovg@ornl.gov>
2022-08-15 16:50:58 -0400

    lmod

--- handsOn1/hello_cades_slurm.sh
+++ handsOn1/hello_cades_slurm.sh
@@ -13,7 +13,7 @@ cd ~/R4HPC/handsOn1
 pwd
 
 source /software/cades-open/spack-envs/base/root/linux-centos7-x86_64/gcc-6.3.0/lmod-8.5.6-wdngv4jylfvg2j6jt7xrtugxggh5lpm5/lmod/lmod/init/bash
-
+export MODULEPATH=/software/cades-open/spack-envs/base/modules/site/Core:/software/cades-open/modulefiles/core
 
 ## module names can vary on different platforms
 module load PE-gnu/3.0     # cades condo
