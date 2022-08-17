fce0eb6f0f1c6fae6f5df946abefdfad0badc118
go-ski <ostrouchovg@ornl.gov>
2022-08-17 17:31:08 -0400

    packages rename

--- code_0/packages_karolina_pbs.sh
+++ code_0/packages_karolina_pbs.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
-#PBS -N package
-#PBS -A DD-21-42
-#PBS -l select=1
-#PBS -l walltime=00:05:00
-#PBS -q qprod
-#PBS -e package.e
-#PBS -o package.o
 
-cat $BASH_SOURCE 
-cd ~/R4HPC/code_1
+cd ~/R4HPC/code_0
 pwd
 
 ## module names can vary on different platforms
 module load R
 echo "loaded R"
-
-## prevent warning when fork is used with MPI
-export OMPI_MCA_mpi_warn_on_fork=0
-export RDMAV_FORK_SAFE=1
-
-# Fix for warnings from libfabric/1.12 on Karolina
-module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore-11.2.0 
 
 Rscript package_installs.R
