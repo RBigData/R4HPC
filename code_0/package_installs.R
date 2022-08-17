9b70db4e6de7e54ef42cfd7bfab10c98d96bf01c
go-ski <ostrouchovg@ornl.gov>
2022-08-17 12:36:37 -0400

    add repo

--- code_0/package_installs.R
+++ code_0/package_installs.R
@@ -1,7 +1,7 @@
 
-install.packages("remotes")
-install.packages("flexiblas")
-install.packages("randomForest")
-install.packages("mlbench")
-install.packages("memuse")
+install.packages("remotes", repos = "https://cran.rstudio.com/")
+install.packages("flexiblas", repos = "https://cran.rstudio.com/")
+install.packages("randomForest", repos = "https://cran.rstudio.com/")
+install.packages("mlbench", repos = "https://cran.rstudio.com/")
+install.packages("memuse", repos = "https://cran.rstudio.com/")
 remotes::install_github("RBigData/pbdMPI")
