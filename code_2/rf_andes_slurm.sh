e70a38dd8f44c8a34c283961e6d6442fc6b6cff8
go-ski <ostrouchovg@ornl.gov>
2022-08-16 22:47:30 -0400

    clean up and print

--- code_2/rf_andes_slurm.sh
+++ code_2/rf_andes_slurm.sh
@@ -3,28 +3,30 @@
 #SBATCH -A ccsd
 #SBATCH -p burst
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
-#SBATCH --ntasks-per-node=1
 #SBATCH -t 00:40:00
-#SBATCH --mem=120G
+#SBATCH --mem=0
 #SBATCH -e ./rf.e
 #SBATCH -o ./rf.o
+#SBATCH --open-mode=truncate
 
 cd ~/R4HPC/code_2
 pwd
 
-module load PE-gnu/4.0
-module load R/4.1
-echo "loaded R"
+## modules are specific to andes.olcf.ornl.gov
+module load openblas/0.3.17-omp
+module load flexiblas
+flexiblas add OpenBLAS $OLCF_OPENBLAS_ROOT/lib/libopenblas.so
+export LD_PRELOAD=$OLCF_FLEXIBLAS_ROOT/lib64/libflexiblas.so
+module load r
+echo -e "loaded R with FlexiBLAS"
 module list
 
 time Rscript rf_serial.r
 time Rscript rf_mc.r --args 1
-#time Rscript rf_mc.r --args 2
-#time Rscript rf_mc.r --args 4
-#time Rscript rf_mc.r --args 8
-#time Rscript rf_mc.r --args 16
-#time Rscript rf_mc.r --args 32
-#time Rscript rf_mc.r --args 64
+time Rscript rf_mc.r --args 2
+time Rscript rf_mc.r --args 4
+time Rscript rf_mc.r --args 8
+time Rscript rf_mc.r --args 16
+time Rscript rf_mc.r --args 32
+time Rscript rf_mc.r --args 64
 
