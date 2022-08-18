67909b7b0c598c96a5da5d2a2cdd3ff2454c5675
go-ski <ostrouchovg@ornl.gov>
2022-08-17 22:44:21 -0400

    Part 1 update

--- new-cades-LMOD.sh
+++
@@ -1,2 +0,0 @@
-source /software/cades-open/spack-envs/base/root/linux-centos7-x86_64/gcc-6.3.0/lmod-8.5.6-wdngv4jylfvg2j6jt7xrtugxggh5lpm5/lmod/lmod/init/bash
-export MODULEPATH=/software/cades-open/spack-envs/base/modules/site/Core:/software/cades-open/modulefiles/core
