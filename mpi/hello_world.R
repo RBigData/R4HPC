d17fe9e4556bfdb78530b26cd8ebd7fca4f3771d
go-ski <ostrouchovg@ornl.gov>
2022-08-18 12:51:01 -0400

    rename

--- mpi/hello_world.R
+++ mpi/hello_world.R
@@ -2,7 +2,7 @@ suppressMessages(library(pbdMPI))
 
 my_rank = comm.rank()
 ranks = comm.size()
-msg = paste0("Hello World! My name is Empi", my_rank,
+msg = paste0("Hello World! My name is Rank", my_rank,
              ". We are ", ranks, " identical siblings.")
 cat(msg, "\n")
 
