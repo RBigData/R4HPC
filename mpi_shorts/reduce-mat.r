080fcd6642e5b557794f13f78634973b4de299f4
go-ski <ostrouchovg@ornl.gov>
2022-08-19 14:28:18 -0400

    reduce only

--- mpi_shorts/reduce-mat.r
+++ mpi_shorts/reduce-mat.r
@@ -4,7 +4,7 @@ x <- matrix( 10*comm.rank() + (1:6), nrow = 2 )
 
 comm.print( x, all.rank = TRUE )
 
-z <- allreduce( x ) # knows it's a matrix
+z <- reduce( x ) # knows it's a matrix
 
 comm.print( z, all.rank = TRUE )
 
