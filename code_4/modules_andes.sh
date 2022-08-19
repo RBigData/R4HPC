90233afe07d5a005d841b5ef7068d72867394f4f
go-ski <ostrouchovg@ornl.gov>
2022-08-19 11:23:04 -0400

    modules

---
+++ code_4/modules_andes.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## assumes this repository was cloned in your home area
+cd ~/R4HPC/code_4
+pwd
+
+## modules are specific to andes.olcf.ornl.gov
+module load openblas/0.3.17-omp
+module load hdf5
+module load flexiblas
+flexiblas add OpenBLAS $OLCF_OPENBLAS_ROOT/lib/libopenblas.so
+export LD_PRELOAD=$OLCF_FLEXIBLAS_ROOT/lib64/libflexiblas.so
+module load r
+echo -e "loaded R with FlexiBLAS"
+module list
