698d1519cbb98a81bf9412c9ffea57b95b03247b
go-ski <ostrouchovg@ornl.gov>
2022-08-19 13:09:01 -0400

    added dependecies

--- code_4/package_installs.R
+++ code_4/package_installs.R
@@ -1,4 +1,8 @@
 
+
+remotes::install_github("RBigData/pbdSLAP")
+remotes::install_github("RBigData/pbdBASE")
+remotes::install_github("RBigData/pbdDMAT")
 remotes::install_github("RBigData/pbdML")
 
 if (!require("BiocManager", quietly = TRUE))
