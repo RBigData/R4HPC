6d8e25d2d21ebbf05b7fa55568679a2e4e57eba1
go-ski <ostrouchovg@ornl.gov>
2022-08-19 00:51:04 -0400

    fix object

--- mpi/mnist_read_mpi.R
+++ mpi/mnist_read_mpi.R
@@ -28,15 +28,15 @@ my_train = t(my_train)  # row-major write and column-major read
 
 ## plot for debugging
  if(comm.rank() == 0) {
-   ivals = sample(nrow(train), 36)
+   ivals = sample(nrow(my_train), 36)
    library(ggplot2)
    image = rep(ivals, 28*28)
-   lab = rep(train_lab[ivals], 28*28)
+   lab = rep(my_train_lab[ivals], 28*28)
    image = factor(paste(image, lab, sep = ": "))
    col = rep(rep(1:28, 28), each = length(ivals))
    row = rep(rep(1:28, each = 28), each = length(ivals))
    im = data.frame(image = image, row = row, col = col, 
-                   val = as.numeric(unlist(train[ivals, ])))
+                   val = as.numeric(unlist(my_train[ivals, ])))
    ggplot(im, aes(row, col, fill = val)) + geom_tile() + facet_wrap(~ image)
  }
 
