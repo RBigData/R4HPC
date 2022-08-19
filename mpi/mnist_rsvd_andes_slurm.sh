517479cdb17c019815e4b1c1babde5a6762fde64
go-ski <ostrouchovg@ornl.gov>
2022-08-19 08:49:35 -0400

    run bigger

--- mpi/mnist_rsvd_andes_slurm.sh
+++ mpi/mnist_rsvd_andes_slurm.sh
@@ -20,18 +20,18 @@ module load openblas/0.3.17-omp
 module load flexiblas
 flexiblas add OpenBLAS $OLCF_OPENBLAS_ROOT/lib/libopenblas.so
 export LD_PRELOAD=$OLCF_FLEXIBLAS_ROOT/lib64/libflexiblas.so
-export UCX_LOG_LEVEL=error
+export UCX_LOG_LEVEL=error  # no UCX warn messages
 
 module load r
 echo -e "loaded R with FlexiBLAS"
 module list
 
 time mpirun --map-by ppr:1:node Rscript mnist_rsvd.R
-#time mpirun --map-by ppr:2:node Rscript mnist_rsvd.R
-#time mpirun --map-by ppr:4:node Rscript mnist_rsvd.R
-#time mpirun --map-by ppr:8:node Rscript mnist_rsvd.R
-#time mpirun --map-by ppr:16:node Rscript mnist_rsvd.R
-#time mpirun --map-by ppr:32:node Rscript mnist_rsvd.R
+time mpirun --map-by ppr:2:node Rscript mnist_rsvd.R
+time mpirun --map-by ppr:4:node Rscript mnist_rsvd.R
+time mpirun --map-by ppr:8:node Rscript mnist_rsvd.R
+time mpirun --map-by ppr:16:node Rscript mnist_rsvd.R
+time mpirun --map-by ppr:32:node Rscript mnist_rsvd.R
 
 
 
