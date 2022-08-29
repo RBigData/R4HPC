eb2aaf4e9c61049192d116e4847ebe66d89d2a93
go-ski <ostrouchovg@ornl.gov>
2022-08-28 23:42:35 -0400

    vt too

--- rsvd/mnist_rsvd.R
+++ rsvd/mnist_rsvd.R
@@ -12,11 +12,12 @@ cyclic_train = as.blockcyclic(dmat_train)
 
 comm.print(comm.size())
 t1 = as.numeric(Sys.time())
-rsvd_train = rsvd(cyclic_train, k = 10, q = 3, retu = FALSE, retv = FALSE)
+rsvd_train = rsvd(cyclic_train, k = 10, q = 3, retu = FALSE, retvt = TRUE)
 t2 = as.numeric(Sys.time())
 t1 = allreduce(t1, op = "min")
 t2 = allreduce(t2, op = "max")
 comm.cat("Time:", t2 - t1, "seconds\n")
+comm.cat("dim(V):", dim(rsvd_train$vt), "\n")
 
 comm.cat("rsvd top 10 singular values:", rsvd_train$d, "\n")
 
